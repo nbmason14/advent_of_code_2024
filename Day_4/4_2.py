2c4c3b754aa841ca117d42390f189bacba91118b
masonne <masonne@oregonstate.edu>
2024-12-12 10:52:59 -0600

    making code more efficient

--- Day_4/4_2.py
+++ Day_4/4_2.py
@@ -6,20 +6,15 @@ os.chdir("H:/GitHub/advent_of_code_2024/Day_4/")
 f = open("H:/GitHub/advent_of_code_2024/Day_4/input.txt","r")
 text = f.readlines()
 
-count_1 = 0
+count = 0
 valid_strings = {'MS','SM'}
 for i in range(140):
     for j in range(140):
-        count_2 = 0
         if text[i][j] == "A" and i >= 1 and j >= 1 and i <= 138 and j <= 138:
-            if text[i-1][j-1] + text[i+1][j+1] in valid_strings:
-                count_2 += 1
-            if text[i-1][j+1] + text[i+1][j-1] in valid_strings:
-                count_2 += 1
-        if count_2 == 2:
-            count_1 += 1
+            if text[i-1][j-1] + text[i+1][j+1] in valid_strings and text[i-1][j+1] + text[i+1][j-1] in valid_strings:
+                count+=1
 
-print(count_1)
+print(count)
 
 
 
